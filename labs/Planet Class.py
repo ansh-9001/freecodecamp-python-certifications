9315b442d5947c9622168f502cc422c365e13261
Ansh <anshprithviraj@gmail.com>
2026-08-06 16:35:52 +0530

    Create Planet Class.py

---
+++ labs/Planet Class.py
@@ -0,0 +1,30 @@
+class Planet:
+    def __init__(self,name,planet_type,star):
+        if type(name) != str or type(planet_type) != str or type(star) != str:
+            raise TypeError('name, planet type, and star must be strings')
+        
+        if name == '' or planet_type == '' or star == '':
+            raise ValueError('name, planet_type, and star must be non-empty strings')
+
+        self.name = name
+        self.planet_type = planet_type
+        self.star = star
+    
+    def orbit(self):
+        return f'{self.name} is orbiting around {self.star}...'
+
+    def __str__(self):
+        return f'Planet: {self.name} | Type: {self.planet_type} | Star: {self.star}'
+
+planet_1 = Planet('M','L','A')
+planet_2 = Planet('V','G','B')
+planet_3 = Planet('E','S','C')
+
+print(planet_1)
+print(planet_2)
+print(planet_3)
+
+print(planet_1.orbit())
+print(planet_2.orbit())
+print(planet_3.orbit())
+
